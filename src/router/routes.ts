d4d3013a69c2bb17debfcdd2c5480fe1f5ea9c0a
Alexnander Nasokin <alexnas2python@gmail.com>
2023-11-13 22:59:36 +0300

    Clean

---
+++ src/router/routes.ts
@@ -0,0 +1,21 @@
+import type { RouteRecordRaw } from 'vue-router'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'home',
+    component: () => import('@/views/HomeView.vue')
+  },
+  {
+    path: '/',
+    name: 'home',
+    component: () => import('@/views/HomeView.vue')
+  }
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import('@/views/AboutView.vue')
+  }
+]
+
+export default routes
